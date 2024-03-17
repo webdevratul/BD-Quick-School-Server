3b7c1af652482dde8a873e311df6c5cd876b4334
webdevratul <mahibulislamratul@gmail.com>
2024-03-17 10:41:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require("./src/App");
+const { mongodbConnection } = require("./src/DatabaseConfig/Db");
+const { PortNumber } = require("./src/Secret");
+
+//Server Preview Check
+app.get("/", async (req, res) => {
+  res.status(200).send({ message: "bd school server working good" });
+});
+
+// server running here
+app.listen(PortNumber, async () => {
+  console.log(`Server is Running at http://localhost:${PortNumber}`);
+  await mongodbConnection();
+});
